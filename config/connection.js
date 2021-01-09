e5921c87ba82895e56a653ce9c00634039096885
Kailee Smith <kaileesmith1501@gmail.com>
2021-01-08 22:13:00 -0800

    created config

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'burgers_db',
+    });
+
+    // Start Connection
+
+    connection.connect((err) => {
+        if (err) {
+            console.error(`error connecting: ${err.stack}`);
+            return;
+        }
+        console.log(`connected as id ${connection.threadId}`);
+    });
+
+    // Exporting Connection
+
+    module.exports = connection;
