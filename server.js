aa73ab9e7c0cacc644bd2294553ef832205b92cb
Kailee Smith <kaileesmith1501@gmail.com>
2021-01-09 17:31:46 -0800

    added connection and paths

--- server.js
+++ server.js
@@ -1,1 +1,41 @@
 const express = require("express");
+const exphbs = require('express-handlebars');
+const mysql = require('mysql');
+const path = require('path');
+
+const app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const PORT = process.env.PORT || 8080;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.use(express.static(path.join(__dirname, '/')));
+app.set('view engine', 'handlebars');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'moviePlannerDB',
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error(`error connecting: ${err.stack}`);
+        return; 
+}
+
+    console.log(`connected as id ${connection.threadId}`);
+    });
+
+    // Start our server so that it can begin listening to client requests.
+    
+app.listen(PORT, () =>
+console.log(`Server listening on: http://localhost:${PORT}`)
+);
