b741feb0f04c9e4fa0dee5512ab72d249b4466f4
Kailee Smith <kaileesmith1501@gmail.com>
2021-01-11 21:15:50 -0800

    updated orm.js to include SQL functions

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,64 @@
 const connection = require('./connection.js');
 
+// Helper function for syntax
+const printQuestionMarks = (num) => {
+    const arr = [];
+    for (let i = 0; i < num; i++) {
+        arr.push('?');
+    }
+        return arr.toString();
+    };
+
+    // Object for all SQL statement functions
 const orm = {
-    selectAll(cb){},
-    insertOne(cb){},
-    updateOne(cb){},
-}
+    all(tableInput, cb){
+        const queryString = `SELECT * FROM ${tableInput};`;
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+},
+    insertOne(table, cols, vals, cb) {
+        let queryString = `INSERT INTO ${table}`;
+    
+        queryString += ' (';
+        queryString += cols.toString();
+        queryString += ') ';
+        queryString += 'VALUES (';
+        queryString += printQuestionMarks(vals.length);
+        queryString += ') ';
+
+        console.log(queryString);
+
+    connection.query(queryString, vals, (err, result) => {
+    if (err) {
+        throw err;
+        }
+
+        cb(result);
+    });
+    },
+
+    updateOne(table, objColVals, condition, cb) {
+        let queryString = `UPDATE ${table}`;
+    
+        queryString += ' SET ';
+        queryString += "devoured = true";
+        queryString += ' WHERE ';
+        queryString += condition;
+        queryString += objToSql(objColVals);
+
+        console.log(queryString);
+    connection.query(queryString, (err, result) => {
+    if (err) {
+        throw err;
+        }
+
+        cb(result);
+        });
+    },
+};
 
 module.exports = orm;
