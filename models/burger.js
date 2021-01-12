73c455304f59969e7d7ebb0d1acf74c372d17d1e
Kailee Smith <kaileesmith1501@gmail.com>
2021-01-11 20:13:30 -0800

    added bootstrap, burger img, update to main.handlebars and blank style.css sheet

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,19 @@
+// Import the ORM to create functions that will interact with the database
 const orm =require('../config/orm');
 
-let burger = {}
+let burger = {
+    all(cb) {
+        orm.all('burgers', (res) => cb(res));
+        },
+        // The variables cols and vals are arrays.
+    insert(cols, vals, cb) {
+        orm.insert('burgers', cols, vals, (res) => cb(res));
+    },
 
+    update(objColVals, condition, cb) {
+        orm.update('cats', objColVals, condition, (res) => cb(res));
+    },
+}
+
+// Export the database functions for the controller 
 module.exports = burger;
