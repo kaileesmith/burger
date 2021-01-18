d00adca36a556961ac0a137ec656253cad9928dc
Kailee Smith <kaileesmith1501@gmail.com>
2021-01-17 22:40:00 -0800

    fixed controller to fic bug

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,32 +8,38 @@ const burger = require('../models/burger.js');
 // Create all our routes and set up logic within those routes where required.
 router.get('/', (req, res) => {
   burger.selectAll((data) => {
-    let hbsObject = {
+    const hbsObject = {
       burgers: data,
     };
-    console.log(hbsObject);
+    console.log('hbsObject', hbsObject);
     res.render('index', hbsObject);
   });
 });
 
 router.post('/api/burgers', (req, res) => {
-  burger.insertOne(['burger_name'], [req.body.burger_name], (result) => {
+  burger.insertOne(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
     res.json({ id: result.insertId });
   });
 });
 
 router.put('/api/burgers/:id', (req, res) => {
-    const condition = `id = ${req.params.id}`;
+  const condition = `id = ${req.params.id}`;
 
-    console.log('condition', condition);
+  console.log('condition', condition);
 
-    burger.updateOne(condition, (results) => {
-        if (results.changedRows === 0) 
+  burger.updateOne(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    (result) => {
+      if (result.changedRows === 0) 
         {
-			return res.status(404).end();
+			  return res.status(404).end();
 		}
-		res.status(200).end();
-	});
+    res.status(200).end();
+    }
+	);
 });
 
 // Export routes for server.js to use.
