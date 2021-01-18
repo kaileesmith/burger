12bb6755d9ad92bd671e1f42af96d5cd86f17ef3
Kailee Smith <kaileesmith1501@gmail.com>
2021-01-17 21:56:59 -0800

    added fetch request and reurn on response

--- public/burgs.js
+++ public/burgs.js
@@ -10,4 +10,30 @@ const devour = document.querySelectorAll(".devBtn");
  // Set up the event listener for the devour button
 if (devour) {
 	devour.forEach((button) => {
-		button.addEventListener('click', (e) => {};
+		button.addEventListener('click', (e) => {
+			e.preventDefault();
+
+			const id = e.currentTarget.getAttribute("data-id");
+			const eaten = {
+                devoured: true,
+			};
+			console.log(id);
+
+			fetch(`/api/burgers/${id}`, {
+                method: 'PUT',
+                headers: {
+                	Accept: 'application/json',
+                	'Content-Type': 'application/json',
+				},
+				
+				body: JSON.stringify(eaten),
+			}).then((response) => 
+			{
+				if (response.ok) 
+				{
+					location.reload("/");
+				} else {
+					alert("Something went wrong!");
+				}
+			});
+		};
