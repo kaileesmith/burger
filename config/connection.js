eb13daa57fcd1da78f84530715e6c5ecc72bcf1b
Kailee Smith <kaileesmith1501@gmail.com>
2021-01-20 20:35:11 -0800

    added heroku requirements

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,19 @@ const connection = mysql.createConnection({
     database: 'burgers_db',
     });
 
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'hacktheplanet',
+        database: 'todoagain_db',
+    });
+};
+
     // Start Connection
+
 
     connection.connect((err) => {
         if (err) {
